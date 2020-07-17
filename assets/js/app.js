794cb23599a2c70862abdeb0db42a7ab7f23d8d0
PierreTruchot <pierre.truchot2@gmail.com>
2020-07-17 16:38:02 +0200

    initialize React / Create app.js & Home.jsx

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import {Home} from './components/Home';
+    
+ReactDOM.render(<Router><Home /></Router>, document.getElementById('root'));
