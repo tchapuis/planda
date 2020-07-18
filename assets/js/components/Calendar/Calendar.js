9fdb6cdedd4f6202955bdf5f4bb8765e93d85fda
PierreTruchot <pierre.truchot2@gmail.com>
2020-07-18 10:59:57 +0200

    feat(Calendar) add eventSelect func

--- assets/js/components/Calendar/Calendar.js
+++ assets/js/components/Calendar/Calendar.js
@@ -1,20 +1,62 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin  from '@fullcalendar/timegrid'
+import classNames from 'classnames';
 
 const MONDAY = 1;
 
 export function Calendar(){
+    const [events] = React.useState([
+        { id:1, title: 'Disponible', date: '2020-07-19T09:30:00+02:00' },
+        { id:2, title: 'Disponible', date: '2020-07-19T10:00:00+02:00' },
+        { id:3, title: 'Disponible', date: '2020-07-19T10:30:00+02:00' }
+    ]);
+    
+    const [selectedEventId, setSelectedEventId] = React.useState(null);
+
+    const eventClick = ({ event }) => {
+       if(selectedEventId ===  event.id ) {
+           return setSelectedEventId(null)
+        }
+       return setSelectedEventId(event.id)
+    }
+
+    function renderEventContent(eventInfo) {
+        const { timeText, event} = eventInfo
+        const isEventSelected = event.id === selectedEventId;
+        const eventClasses = classNames(
+            'planda-event', 
+            { 'planda-event__selected': isEventSelected }
+        )
+        return (
+          <div className={eventClasses}>
+            <b>{timeText}</b>
+            <i>{event.title}</i>
+          </div>
+        )
+      }
+
     return (
         <FullCalendar
+            height={600}
             firstDay={MONDAY}
             locale='fr'
-            plugins={[ dayGridPlugin ]}
-            initialView="dayGridWeek"
-            events={[
-                { title: 'event 1', date: '2019-04-01' },
-                { title: 'event 2', date: '2019-04-02' }
-            ]}
+            plugins={[ timeGridPlugin  ]}
+            initialView="timeGridWeek"
+            nowIndicator={true}
+            slotDuration={'00:30:00'}
+            slotMinTime={'09:30:00'}
+            slotMaxTime={'20:00:00'}
+            defaultTimedEventDuration={'00:30:00'}
+            slotLabelFormat={{
+                hour: 'numeric',
+                minute: '2-digit',
+                omitZeroMinute: true,
+                meridiem: 'short'
+              }}
+            eventContent={renderEventContent}
+            events={events}
+            eventClick={eventClick}
         />
     )
   }
